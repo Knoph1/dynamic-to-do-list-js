86af5d2257dd6623009953a49fa6bc0917a1fbc9
knoph1 <knopholuoch@yahoo.com>
2025-01-13 00:47:20 +0300

    Final task_4.sql

--- Documents/ALX B-E/Intro_to_DB/task_4.sql
+++ Documents/ALX B-E/Intro_to_DB/task_4.sql
@@ -47,3 +47,8 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
+
+-- Query to check the description of the Books table without using DESCRIBE or ANALYZE
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_DEFAULT
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Books';
