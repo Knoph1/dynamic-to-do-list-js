976427932d60da718155e6c529b83c2340119867
knoph1 <knopholuoch@yahoo.com>
2025-01-13 00:31:21 +0300

    Rectifying task_4.sql

--- Documents/ALX B-E/Intro_to_DB/task_4.sql
+++ Documents/ALX B-E/Intro_to_DB/task_4.sql
@@ -1,14 +1,15 @@
--- Query to get the full description of the 'Books' table
--- without using DESCRIBE or EXPLAIN
+-- Use the alx_book_store database
 USE alx_book_store;
 
+-- Query to get the description of the 'Books' table 
+-- without using DESCRIBE or EXPLAIN
 SELECT 
-  COLUMN_NAME, 
-  COLUMN_TYPE, 
-  IS_NULLABLE, 
-  COLUMN_KEY, 
-  COLUMN_DEFAULT, 
-  EXTRA
+  COLUMN_NAME AS 'Column Name', 
+  COLUMN_TYPE AS 'Data Type', 
+  IS_NULLABLE AS 'Is Nullable', 
+  COLUMN_KEY AS 'Key', 
+  COLUMN_DEFAULT AS 'Default Value', 
+  EXTRA AS 'Extra Information'
 FROM 
   INFORMATION_SCHEMA.COLUMNS
 WHERE 
