641f7980b0e6dfbe9aebedae20691b369a3b4801
knoph1 <knopholuoch@yahoo.com>
2025-01-13 00:27:18 +0300

    Rectifying task_2.sql

--- Documents/ALX B-E/Intro_to_DB/task_2.sql
+++ Documents/ALX B-E/Intro_to_DB/task_2.sql
@@ -38,7 +38,7 @@ CREATE TABLE Order_Details (
   order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
   order_id INT,
   book_id INT,
-  quantity INT NOT NULL,
+  quantity DOUBLE NOT NULL, -- Updated data type to DOUBLE
   FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
   FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
 );
