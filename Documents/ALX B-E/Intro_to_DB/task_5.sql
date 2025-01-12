a42fac91315424964e6b8002769635e614a6594a
knoph1 <knopholuoch@yahoo.com>
2025-01-13 00:49:10 +0300

    Final task_5.sql

--- Documents/ALX B-E/Intro_to_DB/task_5.sql
+++ Documents/ALX B-E/Intro_to_DB/task_5.sql
@@ -1,5 +1,5 @@
--- Insert a single row into the customer table
-USE alx_book_store;
+-- task_5.sql
 
-INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Inserting a single row into the customer table
+INSERT INTO customer (customer_id, first_name, last_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
